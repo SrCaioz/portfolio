c97c991973ff880647d3d6b1e12a84d706cc9661
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-06-23 18:58:45 -0300

    fix import navbar for header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Header from "../Components/Header";
 
 import BackGround from "../Components/BackGround";
-import Navbar from "../Components/Navbar";
+
 
 export default function Home() {
 	return (
 		<div className="relative h-screen w-screen overflow-hidden">
 			<BackGround />
-			<Navbar />
+			<Header className={"absolute w-full -top-0.5"}/>
 			<p className="relative text-white text-2xl  -top-8">
 				Conteúdo que fica por cima 🌟
 			</p>
