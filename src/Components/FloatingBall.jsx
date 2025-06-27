3393138cee549edc194add2b21e00f2cf4beb51a
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-06-27 13:02:52 -0300

    add FloatingBall

---
+++ src/Components/FloatingBall.jsx
@@ -0,0 +1,7 @@
+export default function FloatingBall({ className }) {
+    return (
+        <div id="FloatingBall" className={className}>
+            <img src="../assets/static/ftcortada" alt="" srcset="../assets/static/ftcortada.png" className="h-24 w-24"/>
+        </div>
+    )
+}
