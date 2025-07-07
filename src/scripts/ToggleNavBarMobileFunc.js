a15fe579aeaba4388c7c081cc1b0f618ba831634
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-07-07 10:03:37 -0300

    Add: Func togglenavbar mobile

---
+++ src/scripts/ToggleNavBarMobileFunc.js
@@ -0,0 +1,5 @@
+export default function ToggleNavBarMobileFunc() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
