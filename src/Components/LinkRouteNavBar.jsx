42c7c7430743f401f3fdfce410a8334ae8622fde
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-07-08 12:13:53 -0300

    Add: Link Route

---
+++ src/Components/LinkRouteNavBar.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router";
+import Lottie from "lottie-react";
+import { useRef } from "react";
+
+export default function LinkRoutNavBar({ Route, Text, MyImageNavBar }) {
+	const lottieRef = useRef();
+	const handleMouseEnter = () => {
+		lottieRef.current.play();
+	};
+	const handleMouseLeave = () => {
+		lottieRef.current.stop();
+	};
+
+	return (
+		<li
+			className="flex text-center items-center"
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+		>
+			<Link
+				to={{
+					pathname: `${Route}`,
+				}}
+				className="flex justify-evenly items-center w-full text-white text-xl rounded-2xl hover:bg-blue-400 hover:opacity-65"
+			>
+				<Lottie
+					className="w-14"
+					lottieRef={lottieRef}
+					animationData={MyImageNavBar}
+					loop
+					autoplay={false}
+				/>
+				{Text}
+			</Link>
+		</li>
+	);
+}
