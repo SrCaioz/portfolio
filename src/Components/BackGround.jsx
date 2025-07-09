67609bd0fbe47846915d617dfb262ab667855818
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-07-09 13:55:04 -0300

    Update: BG fixed
    Fixei o BG para que não tenha que ficar colocando varios BG e acabar quebrando a página

--- src/Components/BackGround.jsx
+++ src/Components/BackGround.jsx
@@ -36,9 +36,9 @@ export { Divdad, Topbg, Bottombg }
  */
 export default function BackGround() {
 	return (
-		<div className="relative inset-0 h-screen w-screen overflow-hidden transition-colors duration-200 bg-linear-to-br from-slate-700 from-5% to-95% to-white -z-50 dark:bg-linear-to-br dark:to-black dark:transition-colors dark:duration-200">
-			<div className="absolute -left-40 -top-40 h-96 w-9/12 rounded-full blur-3xl bg-linear-to-br from-green to-blue -z-40"></div>
-			<div className="absolute -right-40 -bottom-40 h-96 w-9/12 rounded-full blur-3xl bg-linear-to-br from-blue to-green -z-40"></div>
+		<div className="fixed inset-0 min-h-screen w-full overflow-hidden transition-colors duration-200 bg-gradient-to-b from-slate-700 from-5% to-95% to-white dark:from-black dark:via-slate-900 dark:to-black animate-pulse -z-50">
+			<div className="absolute -left-40 -top-40 h-96 w-9/12 rounded-full blur-3xl bg-gradient-to-br from-green to-blue animate-pulse -z-40"></div>
+			<div className="absolute -right-40 -bottom-40 h-96 w-9/12 rounded-full blur-3xl bg-gradient-to-br from-blue to-green animate-pulse -z-40"></div>
 		</div>
 	);
 }
