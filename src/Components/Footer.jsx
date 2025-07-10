d34fbc6930fea51ffe5d7384c9c8d863fa8cd7aa
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-07-10 10:25:16 -0300

    Update: Footer add animate.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,20 @@
 import HoverFunc from "../scripts/HoverFunc";
 import Github from "../assets/animations/githubjson.json";
 import Email from "../assets/animations/email.json"
+import Linkedin from "../assets/animations/linkedin.json"
+import 'animate.css';
 
 export default function Footer() {
 	return (
 		<footer className="absolute -bottom-1 w-screen rounded-t-md bg-gray-400 dark:bg-gray-700">
-			<div className="flex justify-center align-middle">
-				<a href="mailto:caiocezar.domingosdeoliveira@gmail.com" rel="noopener noreferrer">
+			<div className="flex justify-center items-center ">
+				<a href="https://www.linkedin.com/in/caio-cezar-domingos-de-oliveira-01b389228/" target="_blank" rel="noopener noreferrer" className="animate__animated animate__repeat-1 animate__fadeInDown">
+					<HoverFunc myImages={Linkedin} />
+				</a>
+				<a href="mailto:caiocezar.domingosdeoliveira@gmail.com" rel="noopener noreferrer" className="animate__animated animate__repeat-1 animate__fadeInDown">
 					<HoverFunc myImages={Email} />
 				</a>
-				<a href="https://github.com/Skarzyll" target="_blank" rel="noopener noreferrer">
+				<a href="https://github.com/Skarzyll" target="_blank" rel="noopener noreferrer" className="animate__animated animate__repeat-1 animate__fadeInDown">
 					<HoverFunc myImages={Github} />
 				</a>
 			</div>
