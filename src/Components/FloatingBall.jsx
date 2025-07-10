f6dbd64dcc7ca9eda592c80eb2473d9f40389e6c
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-07-10 10:21:52 -0300

    Update: FloatingBall

--- src/Components/FloatingBall.jsx
+++ src/Components/FloatingBall.jsx
@@ -1,7 +1,9 @@
-export default function FloatingBall({ className }) {
+import ftcortada from '../assets/static/ftcortada3.png';
+
+export default function FloatingBall() {
     return (
-        <div id="FloatingBall" className={className}>
-            <img src="../assets/static/ftcortada" alt="" srcset="../assets/static/ftcortada.png" className="h-24 w-24"/>
+        <div id="FloatingBall" className="absolute w-72 h-72 top-32 bg-blue shadow-md shadow-black right-1/5 rotate-45 overflow-hidden">
+            <img src={ftcortada} alt="" className="h-72 w-72 -rotate-45 mt-10 ml-6"/> 
         </div>
     )
 }
