4694a090462057c95c0e375145d15cedd519df13
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-07-11 08:22:56 -0300

    Update: main.jsx add route about and projects

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter ,Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
 		<StrictMode>
       <Routes>
         <Route exect path="/" element={<App />} />
+        <Route path="/Sobre" element={<About />} />
+        <Route path="/Projetos" element={<Projects />} />
       </Routes>
     </StrictMode>
 	</BrowserRouter>
